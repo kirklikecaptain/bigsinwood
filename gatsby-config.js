5efe8642dded81d506378f3376da5466f3d8ed45
Kirk Hodgman <kjhodgman@gmail.com>
2019-11-28 16:02:19 -0600

    remove comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,20 +14,5 @@ module.exports = {
         accessToken: 'oqa8sChGi6gjZa1fPkg3C6pKrhHvG3-hSiC6wk_vOXs'
       }
     }
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `Big Sin Wood`,
-    //     short_name: `Big Sign Wood`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`
-    //     // icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-    //   }
-    // }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ]
 };
