d63939a1255b7607b59f5c8ab82213cc0ba1db27
Kirk Hodgman <kjhodgman@gmail.com>
2019-11-29 10:33:24 -0600

    add page view

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import '../style/_index.less';
 
 const Meta = ({ title, description }) => {
+  useEffect(() => {
+    window.analytics && window.analytics.page();
+  }, []);
   return (
     <Helmet>
       <title>{title}</title>
