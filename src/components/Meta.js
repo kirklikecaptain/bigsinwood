5e80e1158eabf8de2167a3edbaad1682c5b44feb
Kirk Hodgman <kjhodgman@gmail.com>
2020-02-02 20:43:41 -0800

    add logo assets

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
+import PropTypes from 'prop-types';
 import '../style/_index.less';
+import defaultImage from '../assets/big-sin-wood-meta.jpg';
 
-const Meta = ({ title, description }) => {
+const Meta = ({ title, description, canonical, ogImage, noIndex }) => {
   useEffect(() => {
     window.analytics && window.analytics.page();
   }, []);
@@ -11,8 +13,31 @@ const Meta = ({ title, description }) => {
       <title>{title}</title>
       <meta name='description' content={description} />
       <meta name='google-site-verification' content='IMLrmnyDryrkko7ihyr43HP-meqtnBaIz3B1cW5kU7k' />
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content={title} />
+      <meta name='og:description' content={description} />
+      <meta property='og:url' content={canonical} />
+      <meta property='og:site_name' content='BIG SIN WOOD' />
+      <meta property='og:image' content={ogImage} />
+      <meta name='twitter:image' content={ogImage} />
+      <meta name='twitter:site' content='@bigsinwood' />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={description} />
+      <meta name='twitter:card' content='summary_large_image' />
+      {noIndex && <meta name='robots' content='noindex' />}
     </Helmet>
   );
+};
+
+Meta.defaultProps = {
+  ogImage: `https://www.bigsinwood.com${defaultImage}`
+};
+
+Meta.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  canonical: PropTypes.string.isRequired,
+  ogImage: PropTypes.string
 };
 
 export default Meta;
--- src/views/_home/HomePage.js
+++ src/views/_home/HomePage.js
@@ -1,29 +1,34 @@
 import React, { Fragment } from 'react';
 import Meta from '../../components/Meta';
+import mainLogo from './big-sin-wood-landing.png';
 import './style.less';
 
 const HomePage = () => {
   return (
     <Fragment>
-      <Meta title='BIG SIN WOOD' description='Handcrafted Furniture' />
+      <Meta
+        title='BIG SIN WOOD'
+        description='Handcrafted Furniture'
+        canonical='https://www.bigsinwood.com'
+      />
       <main>
         <div className='message'>
-          <p>Welcome to</p>
-          <h1>
-            <span>BIG SIN WOOD</span>
-          </h1>
-          <p>The new web portal is coming soon!</p>
-          <p>
-            In the meantime... visit us on{' '}
-            <a
-              href='https://www.instagram.com/bigsinwood/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              IG
-            </a>
-          </p>
-          <p className='tag'>"Yall come back now, ya hear"</p>
+          <div className='text'>
+            <div>Welcome to</div>
+            <img src={mainLogo} alt='BIG SIN WOOD' />
+            <p>The new web portal is coming soon!</p>
+            <p>
+              In the meantime... visit us on{' '}
+              <a
+                href='https://www.instagram.com/bigsinwood/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                IG
+              </a>
+            </p>
+            <p className='tag'>"Yall come back now, ya hear"</p>
+          </div>
         </div>
       </main>
     </Fragment>
